69987236d9723ee4f8e5a2aea37b10b7b513a3b3
gabrielboostphoenix <mr.boostphoenix@gmail.com>
2024-02-22 16:43:10 -0300

    Updating a File
    some properties seem to be missing in certain cases

--- src/types/movieDetails.ts
+++ src/types/movieDetails.ts
@@ -1,13 +1,26 @@
 // This is the movie details type
 interface movieDetails {
     adult: boolean,
-    backdrop_path: string,
-    belongs_to_collection: null,
+    backdrop_path: string | null,
+    belongs_to_collection: {
+        id: number,
+        name: string,
+        poster_path: string | null,
+        backdrop_path: string | null
+    }[] | {
+        id: number,
+        name: string,
+        poster_path: string | null,
+        backdrop_path: string | null
+    } | null,
     budget: number,
     genres: {
         id: number,
         name: string
-      }[],
+    }[] | {
+        id: number,
+        name: string
+    } | null,
     homepage: string,
     id: number,
     imdb_id: string,
@@ -15,25 +28,29 @@ interface movieDetails {
     original_title: string,
     overview: string,
     popularity: number,
-    poster_path: string,
+    poster_path: string | null,
     production_companies: {
         id: number,
-        logo_path: string,
+        logo_path: string | null,
         name: string,
         origin_country: string
-    }[],
+    }[] | {
+        id: number,
+        logo_path: string | null,
+        name: string,
+        origin_country: string
+    } | null,
     production_countries: {
         iso_3166_1: string,
         name: string
-    }[],
+    }[] | {
+        iso_3166_1: string,
+        name: string
+    } | null,
     release_date: string,
     revenue: number,
     runtime: number,
-    spoken_languages: {
-        english_name: string,
-        iso_639_1: string,
-        name: string
-    }[],
+    spoken_languages: any,
     status: string,
     tagline: string,
     title: string,
