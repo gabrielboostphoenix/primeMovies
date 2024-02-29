8b3203c7cc2da7160b1c26c7b2935450dc0d3eb1
gabrielboostphoenix <mr.boostphoenix@gmail.com>
2024-02-29 02:15:16 -0300

    Adding and Updating Files
    In this update, i've added a new route that points to sign-up page route. I also done some styles to it mainly to the sign-in form.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 // Importing Area
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
+import { SignUp } from './pages/sign-up-page/SignUp';
 import { MoviesExploring } from './pages/movies-exploring-page/MoviesExploring';
 import { MovieView } from './pages/movie-view-page/MovieView';
 import { Header } from './components/header-component/Header';
@@ -14,6 +15,7 @@ const AppRoutes = () => {
                     <Route path='/' element={ <MoviesExploring /> } />
                     <Route path='/moviesExploring' element={ <MoviesExploring /> } />
                     <Route path='/movieView/:movieID' element={ <MovieView /> } />
+                    <Route path='/signUp' element={ <SignUp /> } />
                 </Routes>
             </BrowserRouter>
         </>
---
+++ src/pages/sign-up-page/SignUp.tsx
@@ -0,0 +1,23 @@
+// Importing Area
+import Style from './SignUp.module.css';
+import { useState, useEffect } from 'react';
+
+// That's the sign up page component
+const SignUp = () => {
+    return (
+        <main className={Style.container}>
+            <form className={Style.formContainer}>
+                <label htmlFor='userName' className={Style.userNameLabel}>Nome:</label>
+                <input type='text' id='userName' className={Style.userNameField} placeholder='Digite o seu nome de usuário' />
+                <label htmlFor="userEmail" className={Style.userEmailLabel}>E-mail:</label>
+                <input type='email' id='userEmail' className={Style.userEmailField} placeholder='Coloque o seu endereço de e-mail' />
+                <label htmlFor='userPassword' className={Style.userPasswordLabel}>Senha:</label>
+                <input type="text" id='userPassword' className={Style.userPasswordField} placeholder='Crie uma senha para a sua conta' />
+                <button type='submit' className={Style.accountCreatingButton}>Criar Conta</button>
+            </form>
+        </main>
+    );
+}
+
+// Exporting Area
+export { SignUp };
