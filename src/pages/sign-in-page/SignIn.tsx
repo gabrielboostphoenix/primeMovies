44ba6fd67e66d1ea16dae497adb0ee3476c00bc4
gabrielboostphoenix <mr.boostphoenix@gmail.com>
2024-05-28 23:46:24 -0300

    Updating Files
    In this commit, I've adding some styles for different screens widths where I only didn't added for devices with a large width like laptops and notebooks or other monitors of computers and TV smart's

--- src/pages/sign-in-page/SignIn.tsx
+++ src/pages/sign-in-page/SignIn.tsx
@@ -4,7 +4,9 @@ import Style from './SignIn.module.css';
 // That's the sign in page component
 const SignIn = () => {
     return (
+
         <main className={Style.container}>
+
             <form className={Style.formContainer}>
                 <label htmlFor='userEmail' className={Style.userEmailLabel}>E-mail:</label>
                 <input type='email' id='userEmail' className={Style.userEmailField} placeholder='Coloque o seu endereço de e-mail'/>
@@ -12,7 +14,9 @@ const SignIn = () => {
                 <input type='password' id='userPassword' className={Style.userPasswordField} placeholder='Crie uma senha para a sua conta'/>
                 <input type='button' value="Enviar" className={Style.sendButton}/>
             </form>
+
         </main>
+
     );
 }
 
