b9a653a9692d382b05b4203422e70fb261072463
Gabriel Pires <102393858+gabrielboostphoenix@users.noreply.github.com>
2024-06-09 01:29:46 -0300

    Updating some Files
    This commit is relative to the styling of sign-up page where until here it's possible to see the mobile version to small devices with a screen width between 320 pixels until 400 pixels.

--- src/pages/sign-up-page/SignUp.tsx
+++ src/pages/sign-up-page/SignUp.tsx
@@ -5,17 +5,32 @@ import { useState, useEffect } from 'react';
 // That's the sign up page component
 const SignUp = () => {
     return (
+
         <main className={Style.container}>
+
             <form className={Style.formContainer}>
-                <label htmlFor='userName' className={Style.userNameLabel}>Nome:</label>
-                <input type='text' id='userName' className={Style.userNameField} placeholder='Digite o seu nome de usuário' />
-                <label htmlFor="userEmail" className={Style.userEmailLabel}>E-mail:</label>
-                <input type='email' id='userEmail' className={Style.userEmailField} placeholder='Coloque o seu endereço de e-mail' />
-                <label htmlFor='userPassword' className={Style.userPasswordLabel}>Senha:</label>
-                <input type='password' id='userPassword' className={Style.userPasswordField} placeholder='Crie uma senha para a sua conta' />
+
+                <div className={Style.nameContainer}>
+                    <label htmlFor='userName' className={Style.userNameLabel}>Nome:</label>
+                    <input type='text' id='userName' className={Style.userNameField} placeholder='Digite o nome de usuário' />
+                </div>
+
+                <div className={Style.emailContainer}>
+                    <label htmlFor="userEmail" className={Style.userEmailLabel}>E-mail:</label>
+                    <input type='email' id='userEmail' className={Style.userEmailField} placeholder='Coloque um endereço de e-mail' />
+                </div>
+
+                <div className={Style.passwordContainer}>
+                    <label htmlFor='userPassword' className={Style.userPasswordLabel}>Senha:</label>
+                    <input type='password' id='userPassword' className={Style.userPasswordField} placeholder='Crie uma senha para conta' />
+                </div>
+
                 <button type='submit' className={Style.accountCreatingButton}>Criar Conta</button>
+
             </form>
+
         </main>
+
     );
 }
 
